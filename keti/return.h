878a61d2af774845eb303bcdabe9c3ab46941efc
ria <choiria@keti.re.kr>
2023-07-11 13:28:00 +0900

    return.h

--- keti/return.h
+++ keti/return.h
@@ -1,16 +1,26 @@
-#include <stdlib.h>
-#include <sstream>
-#include <tuple>
+#pragma once
+#include <iostream>
+#include <unordered_map>
 #include <sys/ipc.h>
 #include <sys/types.h>
 #include <sys/msg.h>
+#include <sys/socket.h>
+#include <netinet/in.h> 
+#include <arpa/inet.h>
+#include <unistd.h>
 
-#include "input.h"
+#include "rapidjson/document.h"
+#include "rapidjson/stringbuffer.h"
+#include "rapidjson/writer.h"
+#include "rapidjson/prettywriter.h"
+
+#include "internal_queue.h"
+#include "config.h"
+#include "keti_type.h"
+#include "keti_log.h"
 
 using namespace rapidjson;
 using namespace std;
-
-#define BUFF_M_PORT 8888
 
 struct MergeResult;
 
@@ -21,49 +31,49 @@ class Return{
         Return(){}
         void ReturnResult();
         void SendDataToBufferManager(MergeResult &mergeResult);
-};
 
-struct message{
-        long msg_type;
-        char msg[2000];
+        inline const static std::string LOGTAG = "CSD Return";
+        char msg[200];
 };
 
 struct MergeResult{
+    int query_id;
     int work_id;
     string csd_name;
-    // vector<pair<int,list<int>>> merged_block_id_list; 
-    // <is full 10 block, block id list>
     vector<int> row_offset; 
-    int rows; 
+    int row_count; 
     char data[BUFF_SIZE];
     int length;
+    vector<int> projection_datatype;
+    vector<int> projection_length;
     int total_block_count;
     int result_block_count;
     int current_block_count;
-    int last_valid_block_id;//전체가 valid라면 == total_block_count일것
-    map<string,vector<Data>> groupby_map;
 
+    MergeResult(){}
     //merge.cc의 최초 생성자
-    MergeResult(int workid_, string csdname_, int total_block_count_){
-        work_id = workid_;
-        csd_name = csdname_;
-        // merged_block_id_list.clear();
+    MergeResult(int query_id_, int work_id_, string csd_name_, 
+    vector<int> projection_datatype_, vector<int> projection_length_,
+    int total_block_count_)
+    : query_id(query_id_),
+      work_id(work_id_),
+      csd_name(csd_name_),
+      projection_datatype(projection_datatype_),
+      projection_length(projection_length_),
+      total_block_count(total_block_count_){
         row_offset.clear();
-        rows = 0;
+        row_count = 0;
         length = 0;
         memset(&data, 0, sizeof(BUFF_SIZE));
-        total_block_count = total_block_count_;
         result_block_count = 0;
         current_block_count = 0;
-        last_valid_block_id = INT_MAX;
     }
 
     void InitMergeResult(){
-        // merged_block_id_list.clear();
         row_offset.clear();
         memset(&data, 0, sizeof(BUFF_SIZE));
-        rows = 0;
+        row_count = 0;
         length = 0;
         result_block_count = 0;
     }
-};
+};  
