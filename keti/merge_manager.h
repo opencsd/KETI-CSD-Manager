8efbedd5069eff26f165d46a0261bbe321892ae1
ria <choiria@keti.re.kr>
2023-07-11 14:08:48 +0900

    merge_manager.h

--- keti/merge_manager.h
+++ keti/merge_manager.h
@@ -1,126 +1,139 @@
-#include <limits.h>
+#pragma once
+#include <stack>
+#include <bitset>
+#include <math.h>
+
 #include "return.h"
 
-using namespace std; 
-
 struct Result;
+struct FilterInfo;
 
 extern WorkQueue<Result> MergeQueue;
 extern WorkQueue<MergeResult> ReturnQueue;
 
+struct T{
+  string varString;
+  int varInt;
+  int64_t varLong;
+  float varFloat;
+  double varDouble;
+  int real_size = 0;//소수점 길이//여기추가
+};
+
+typedef std::pair<int, int> pair_key;
+struct pair_hash{
+    template <class T1, class T2>
+    std::size_t operator() (const std::pair<T1, T2> &pair) const{
+        return std::hash<T1>()(pair.first) ^ std::hash<T2>()(pair.second);
+    }
+};
+
 class MergeManager{
 public:
-    MergeManager(){}
     void Merging();
     void MergeBlock(Result &result);
-    void SendDataToBufferManager(MergeResult &mergedBlock);
+
+    inline const static std::string LOGTAG = "CSD Merge Manager";
+    char msg[200];
 
 private:
-    unordered_map<int, MergeResult> m_MergeManager;// key=workid
+    unordered_map<pair_key, MergeResult, pair_hash> m_MergeManager;// key=<qid,wid>
+
+    pair<T,int> stack_charToValue(char* dest, int type, int len);
+    int stack_valueToChar(char* dest, int dest_type, T value, int type);
+    void getColOffset(const char *origin_row_data, FilterInfo filter_info, int* col_offset);
+    int calculCase(FilterInfo filter_info, char* origin_row_data, int* col_offset, int l, char* dest);
+    int calculSubstring(FilterInfo filter_info, char* origin_row_data, int* col_offset, int l, char* dest);
+    int calculExtract(FilterInfo filter_info, char* origin_row_data, int* col_offset, int l, char* dest);
+    int calculPostfix(vector<string> values, vector<int> types, FilterInfo filter_info, char* origin_row_data, int* col_offset, char* dest, int projection_datatype);
+};
+
+struct Projection{
+    int projection; // KETI_SELECT_TYPE
+    vector<string> values;
+    vector<int> types; // KETI_ValueType
 };
 
 struct FilterInfo{
-    string table_filter;
-    vector<string> table_col;
+    vector<string> table_col;//스캔테이블
     vector<int> table_offset;
     vector<int> table_offlen;
     vector<int> table_datatype;
-    unordered_map<string, int> colindexmap;
-    vector<string> column_filter;
-    vector<vector<string>> column_projection;
-    vector<string> groupby_col;
-    vector<pair<int,string>> orderby_col;
+    unordered_map<string, int> colindexmap;//col index
+    string table_filter;//where절 정보
+    vector<Projection> column_projection;//select절 정보
+    vector<int> projection_datatype;//*컬럼 프로젝션 후 컬럼의 데이터타입
+    vector<int> projection_length;//*컬럼 프로젝션 후 컬럼의 길이
     
     FilterInfo(){}
     FilterInfo(
-        string table_filter_, vector<string> table_col_,
-        vector<int> table_offset_, vector<int> table_offlen_,
-        vector<int> table_datatype_, unordered_map<string, int> colindexmap_,
-        vector<string> column_filter_, vector<vector<string>> column_projection_,
-        vector<string> groupby_col_, vector<pair<int,string>> orderby_col_)
-        : table_filter(table_filter_),
-          table_col(table_col_),
-          table_offset(table_offset_),
-          table_offlen(table_offlen_),
-          table_datatype(table_datatype_),
-          colindexmap(colindexmap_),
-          column_filter(column_filter_),
-          column_projection(column_projection_),
-          groupby_col(groupby_col_),
-          orderby_col(orderby_col_){}
+      vector<string> table_col_, vector<int> table_offset_, 
+      vector<int> table_offlen_, vector<int> table_datatype_,
+      unordered_map<string, int> colindexmap_, string table_filter_,
+      vector<Projection> column_projection_, vector<int> projection_datatype_,
+      vector<int> projection_length_)
+      : table_col(table_col_),
+        table_offset(table_offset_),
+        table_offlen(table_offlen_),
+        table_datatype(table_datatype_),
+        colindexmap(colindexmap_),
+        table_filter(table_filter_),
+        column_projection(column_projection_),
+        projection_datatype(projection_datatype_),
+        projection_length(projection_length_){}
 };
 
 struct Result{
+    int query_id;
     int work_id;
-    // pair<int,list<int>> block_id_list; //<is full 10 block, block list>
+    string csd_name;
+    int total_block_count;
+    FilterInfo filter_info;
     int row_count;
     int length;
     char data[BUFF_SIZE];
     vector<int> row_offset;
-    string csd_name;
-    FilterInfo filter_info;
-    int total_block_count;
-    int result_block_count;
-    list<PrimaryKey> primary_key_list;
-    int last_valid_block_id;//이 id 보다 큰 블록은 유효하지 않음
     vector<vector<int>> row_column_offset;
-    vector<string> column_name;
+    int result_block_count;
 
-    //scan.cc의 최초 생성자
+    //scan, filter의 최초 생성자
     Result(
-        int work_id_, int total_block_count_, 
-        string csd_name_, list<PrimaryKey> primary_key_list_)
-        : work_id(work_id_), 
-          total_block_count(total_block_count_),
-          csd_name(csd_name_), 
-          primary_key_list(primary_key_list_){
-            // block_id_list.first = 1;
-            // block_id_list.second.clear();
-            result_block_count = 0;
-            last_valid_block_id = INT_MAX;
-          } 
-
-    //scan.cc의 filter로 보내는 생성자(scan filter)
-    //scan.cc의 merge로 보내는 생성자(index only scan)
-    Result(
-        int work_id_, /*pair<int,list<int>> block_id_list_,*/
-        int row_count_, int length_, char* data_,
-        vector<int> row_offset_, string csd_name_,
-        FilterInfo filter_info_, int total_block_count_,
-        int result_block_count_, int last_valid_block_id_)
-        : work_id(work_id_), 
-          // block_id_list(block_id_list_),
-          row_count(row_count_), length(length_),
-          row_offset(row_offset_),
-          csd_name(csd_name_),
-          filter_info(filter_info_),
-          total_block_count(total_block_count_),
-          result_block_count(result_block_count_),
-          last_valid_block_id(last_valid_block_id_){
-            memcpy(data, data_, length);
-          }      
-
-    //filter.cc의 최초 생성자
-    Result(
-        int work_id_, /*pair<int,list<int>> block_id_list_,*/
-        int row_count_, int length_, string csd_name_, 
-        FilterInfo filter_info_, int total_block_count_,
-        int result_block_count_, int last_valid_block_id_)
-        : work_id(work_id_), /*block_id_list(block_id_list_),*/ 
-          row_count(row_count_), length(length_),
-          csd_name(csd_name_), 
-          filter_info(filter_info_),
-          total_block_count(total_block_count_),
-          result_block_count(result_block_count_),
-          last_valid_block_id(last_valid_block_id_){} 
+      int query_id_, int work_id_, string csd_name_,
+      int total_block_count_, FilterInfo filter_info_,
+      int result_block_count_ = 0)
+      : query_id(query_id_),
+        work_id(work_id_), 
+        csd_name(csd_name_),
+        total_block_count(total_block_count_),
+        filter_info(filter_info_),
+        result_block_count(result_block_count_){
+          row_count = 0;
+          length = 0;
+          memset(&data, 0, sizeof(BUFF_SIZE));
+          row_offset.clear();
+          row_column_offset.clear();
+        } 
  
   void InitResult(){
-    // block_id_list.second.clear();
     row_count = 0;
     length = 0;
     memset(&data, 0, sizeof(BUFF_SIZE));
     row_offset.clear();
+    row_column_offset.clear();
     result_block_count = 0;
   }
-
 };
+
+inline std::string& rtrim_(std::string& s, const char* t = " \t\n\r\f\v\0"){
+	s.erase(s.find_last_not_of(t) + 1);
+	return s;
+}
+
+inline std::string& ltrim_(std::string& s, const char* t = " \t\n\r\f\v\0"){
+	s.erase(0, s.find_first_not_of(t));
+	return s;
+}
+
+inline std::string& trim_(std::string& s, const char* t = " \t\n\r\f\v\0"){
+	return ltrim_(rtrim_(s, t), t);
+}
